8db5aa75be4e80b72305c467fb36013e7b11611a
Josh Steinberg <jpsteinberg@gmail.com>
2021-05-24 12:21:12 -0400

    installed material ui

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,37 +1,30 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import LogoutButton from './auth/LogoutButton';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import LogoutButton from "./auth/LogoutButton";
+import Button from "@material-ui/core/Button";
 
 const NavBar = () => {
   return (
     <nav>
-      <ul>
-        <li>
-          <NavLink to="/" exact={true} activeClassName="active">
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/login" exact={true} activeClassName="active">
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/sign-up" exact={true} activeClassName="active">
-            Sign Up
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/users" exact={true} activeClassName="active">
-            Users
-          </NavLink>
-        </li>
-        <li>
-          <LogoutButton />
-        </li>
-      </ul>
+      <NavLink to="/" exact={true} activeClassName="active">
+        Discover
+      </NavLink>
+
+      <NavLink to="/login" exact={true} activeClassName="active">
+        Login
+      </NavLink>
+
+      <NavLink to="/sign-up" exact={true} activeClassName="active">
+        Sign Up
+      </NavLink>
+
+      <NavLink exact={true} activeClassName="active" to="/users">
+        <Button variant="contained">Users</Button>
+      </NavLink>
+
+      <LogoutButton />
     </nav>
   );
-}
+};
 
 export default NavBar;
