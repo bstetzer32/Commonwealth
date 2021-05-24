acf22947ff3065d68b36050f8048cd9da56666e8
Josh Steinberg <jpsteinberg@gmail.com>
2021-05-24 18:39:26 -0400

    added category, city, and state routes

--- app/models/city.py
+++ app/models/city.py
@@ -1,22 +1,23 @@
 from .db import db
 
+
 class City(db.Model):
-  __tablename__= "cities"
+    __tablename__ = "cities"
 
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String)
-  state_id = db.Column(db.Integer, db.ForeignKey("states.id"))
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    state_id = db.Column(db.Integer, db.ForeignKey("states.id"))
 
-  state = db.relationship("State", back_populates="cities")
-  users = db.relationship("User", back_populates="city")
-  projects = db.relationship("Project", back_populates="city_relationship")
+    state = db.relationship("State", back_populates="cities")
+    users = db.relationship("User", back_populates="city")
+    projects = db.relationship("Project", back_populates="city_relationship")
 
-  def to_dict(self):
-    return {
-      "id": self.id,
-      "name": self.name,
-      "state_id": self.state_id,
-      "state": self.state,
-      "users":self.users,
-      "projects": self.projects
-    }
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "state_id": self.state_id,
+            "state": self.state,
+            "users": self.users,
+            "projects": self.projects
+        }
