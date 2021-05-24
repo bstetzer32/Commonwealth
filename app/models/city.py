d3f21f2c1863aa262932b542110cccebb24bea9f
Chuks <chuksota@gmail.com>
2021-05-24 18:17:24 -0500

    small edit

--- app/models/city.py
+++ app/models/city.py
@@ -9,9 +9,9 @@ class City(db.Model):
     state_id = db.Column(db.Integer, db.ForeignKey("states.id"))
 
 
-  state = db.relationship("State", back_populates="cities")
-  users = db.relationship("User", back_populates="city_relationship")
-  projects = db.relationship("Project", back_populates="city_relationship")
+    state = db.relationship("State", back_populates="cities")
+    users = db.relationship("User", back_populates="city_relationship")
+    projects = db.relationship("Project", back_populates="city_relationship")
 
     def to_dict(self):
         return {
