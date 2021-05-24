a1561ac5e9d80add44ff49d96e3dfea8448d0f96
Ben Stetzer <benstetzer@gmail.com>
2021-05-24 15:24:09 -0400

    lfeatured recommended styling

--- react-app/src/components/FeaturedRecommended.js
+++ react-app/src/components/FeaturedRecommended.js
@@ -8,56 +8,58 @@ import Filter2Icon from '@material-ui/icons/Filter2';
 import Filter3Icon from '@material-ui/icons/Filter3';
 
 const FeaturedRecommended = () => {
+
     return (
         <Box display="flex" width="100%">
-            <Box width="55%">
-                    <div>Featured Project</div>
-                <Box width="100%">
+            <Box width="45%"  m="5%">
+            <div>Featured Project</div>
+            <br/>
+                <Box width="90%" m="5%">
                     <img src="https://ksr-static.imgix.net/21-05-04Oven_Steel_Lifestyle_Veggie_Chicken1368-4257a65.jpg?ixlib=rb-4.0.2&auto=compress%2Cformat&w=1000&fit=min&s=f81c8fec64231267b46beb06d29bc9c5" object-fit="contain" width="100%"></img>
                     <h1>The Misen Oven Steel</h1>
-                    <h2>Misen wants to make roasting, braising, boiling and searing easier and more delicious with this oven-enhancer.</h2>
+                    <div>Misen wants to make roasting, braising, boiling and searing easier and more delicious with this oven-enhancer.</div>
                     <Box display="flex" justifyContent="space-between">
                     <h3>by: Misen</h3>
                     <h3>32% Funded</h3>
                     </Box>
                 </Box>
             </Box>
-            <Box width="45%" display="flex" flexDirection="column">
+            <Box width="35%" m="5%" display="flex" flexDirection="column">
                 <div>Recomended Projects</div>
-                <Box width="100%" height="30%" display="flex">
+                <Box width="90%" m="5%" height="20%" display="flex">
                     <img src="https://ksr-static.imgix.net/21-05-04Oven_Steel_Lifestyle_Veggie_Chicken1368-4257a65.jpg?ixlib=rb-4.0.2&auto=compress%2Cformat&w=1000&fit=min&s=f81c8fec64231267b46beb06d29bc9c5" object-fit="contain" width="40%"></img>
                     <Box>
-                    <h1>The Misen Oven Steel</h1>
-                    <h2>Misen wants to make roasting, braising, boiling and searing easier and more delicious with this oven-enhancer.</h2>
+                    <h3>The Misen Oven Steel</h3>
+                    <div>Misen wants to make roasting, braising, boiling and searing easier and more delicious with this oven-enhancer.</div>
                     <Box display="flex" justifyContent="space-between">
-                    <h3>by: Misen</h3>
-                    <h3>32% Funded</h3>
+                    <h4>by: Misen</h4>
+                    <h4>32% Funded</h4>
                     </Box>
                     </Box>
                 </Box>
-                <Box width="100%" height="30%" display="flex">
+                <Box width="90%" m="5%" height="20%" display="flex">
                     <img src="https://ksr-static.imgix.net/21-05-04Oven_Steel_Lifestyle_Veggie_Chicken1368-4257a65.jpg?ixlib=rb-4.0.2&auto=compress%2Cformat&w=1000&fit=min&s=f81c8fec64231267b46beb06d29bc9c5" object-fit="contain" width="40%"></img>
                     <Box>
-                    <h1>The Misen Oven Steel</h1>
-                    <h2>Misen wants to make roasting, braising, boiling and searing easier and more delicious with this oven-enhancer.</h2>
+                    <h3>The Misen Oven Steel</h3>
+                    <div>Misen wants to make roasting, braising, boiling and searing easier and more delicious with this oven-enhancer.</div>
                     <Box display="flex" justifyContent="space-between">
-                    <h3>by: Misen</h3>
-                    <h3>32% Funded</h3>
+                    <h4>by: Misen</h4>
+                    <h4>32% Funded</h4>
                     </Box>
                     </Box>
                 </Box>
-                <Box width="100%" height="30%" display="flex">
+                <Box width="90%" m="5%" height="20%" display="flex">
                     <img src="https://ksr-static.imgix.net/21-05-04Oven_Steel_Lifestyle_Veggie_Chicken1368-4257a65.jpg?ixlib=rb-4.0.2&auto=compress%2Cformat&w=1000&fit=min&s=f81c8fec64231267b46beb06d29bc9c5" object-fit="contain" width="40%"></img>
                     <Box>
-                    <h1>The Misen Oven Steel</h1>
-                    <h2>Misen wants to make roasting, braising, boiling and searing easier and more delicious with this oven-enhancer.</h2>
+                    <h3>The Misen Oven Steel</h3>
+                    <div>Misen wants to make roasting, braising, boiling and searing easier and more delicious with this oven-enhancer.</div>
                     <Box display="flex" justifyContent="space-between">
-                    <h3>by: Misen</h3>
-                    <h3>32% Funded</h3>
+                    <h4>by: Misen</h4>
+                    <h4>32% Funded</h4>
                     </Box>
                     </Box>
                 </Box>
-                <Box display="flex" justifyContent="center">
+                <Box display="flex" justifyContent="center" m="5%">
                     <ArrowBackIosIcon/>
                     <Filter1Icon/>
                     <Filter2Icon/>
