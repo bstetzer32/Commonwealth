6ac336ad0cd484e65d5009725bdb5372b3081536
Josh Steinberg <jpsteinberg@gmail.com>
2021-05-24 14:00:02 -0400

    search for navbar

--- react-app/src/components/Search.js
+++ react-app/src/components/Search.js
@@ -1,21 +1,10 @@
 import React from "react";
 import TextField from "@material-ui/core/TextField";
-import { makeStyles } from "@material-ui/core/styles";
 import IconButton from "@material-ui/core/IconButton";
 import InputAdornment from "@material-ui/core/InputAdornment";
 import SearchIcon from "@material-ui/icons/Search";
 
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     "& .MuiTextField-root": {
-//       margin: theme.spacing(1),
-//       width: "25ch",
-//     },
-//   },
-// }));
-
 const SearchBar = () => {
-  //   const classes = useStyles();
   const handleSearch = (e) => {
     e.preventDefault();
   };
