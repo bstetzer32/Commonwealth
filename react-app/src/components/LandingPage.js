7aded00f3344d0fba3ec3c2340a2eb95ae364502
Ben Stetzer <benstetzer@gmail.com>
2021-05-25 16:19:37 -0400

    context

--- react-app/src/components/LandingPage.js
+++ react-app/src/components/LandingPage.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, {useContext, useEffect} from "react";
+import { useDispatch, useSelector } from "react-redux";
 import FeaturedRecommended from './FeaturedRecommended';
 import ScrollBanner from './ScrollBanner';
+import {useParams} from "react-router-dom";
+import {PageContext} from '../context/PageContext'
 
-const LandingPage = () => {
+const LandingPage = ({landingPageType}) => {
+    const dispatch = useDispatch();
+    const {type, id} = useContext(PageContext)
+    const {pageType, setPageType} = type
+    const {pageId, setPageId} = id
+    const {landingPageId} = useParams()
+    useEffect(() => {
+        if (landingPageType !== pageType) {
+            dispatch(setPageType(landingPageType))
+        }
+        if (landingPageId !== pageId) {
+            dispatch(setPageId(landingPageId))
+        }
+    })
     return (<>
         <FeaturedRecommended />
         <ScrollBanner type='Fresh Favorites'/>
