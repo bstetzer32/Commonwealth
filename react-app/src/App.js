f3fee7b9ce6a1e5b5764cb42da0ff2633a55bd0f
Marshall-Diffey <marshalldiffey@yahoo.com>
2021-05-25 12:37:02 -0500

    Render initial project page

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -8,6 +8,7 @@ import ProtectedRoute from "./components/auth/ProtectedRoute";
 import UsersList from "./components/UsersList";
 import User from "./components/User";
 import LandingPage from "./components/LandingPage"
+import ProjectPage from "./components/ProjectPage"
 
 import { authenticate } from "./store/session";
 
@@ -46,6 +47,9 @@ function App() {
         <ProtectedRoute path="/users/:userId" exact={true}>
           <User />
         </ProtectedRoute>
+        <Route path="/project/:id">
+          <ProjectPage />
+        </Route>
       </Switch>
     </BrowserRouter>
   );
