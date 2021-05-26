ced595ab00a73c88d7bdca7e72f98d239b0b6529
Chuks <chuksota@gmail.com>
2021-05-26 16:14:59 -0500

    made logo clickable

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -5,16 +5,17 @@ import Button from "@material-ui/core/Button";
 import SearchBar from "./Search";
 import Discover from "./utils/Menus/Discover";
 import LoginMenu from "./utils/Menus/LoginMenu";
-
+import {useHistory} from 'react-router-dom'
 const NavBar = () => {
+  let history = useHistory()
   return (
     <nav className="nav__bar">
       <div className="nav__element--discover">
         <Discover className="nav__element" />
       </div>
-      <div className="nav__element" id="nav__logo">
+      <NavLink to='/'className="nav__element" id="nav__logo">
         commonwealth
-      </div>
+      </NavLink>
       <div className="nav__element--searchlog">
         <SearchBar id="nav__element--searchBar" />
         <LoginMenu className="nav__element" id="nav__element--loginMenu" />
