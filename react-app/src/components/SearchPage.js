a159e716067f9d22e7671d16446e11e8edaf7aa1
Josh Steinberg <jpsteinberg@gmail.com>
2021-05-26 16:08:59 -0400

    search and show tiles

--- react-app/src/components/SearchPage.js
+++ react-app/src/components/SearchPage.js
@@ -1,11 +1,23 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import SearchPageBar from "./SearchPageBar";
+import ScrollTile from "./ScrollTile";
 
 export default function SearchPage() {
+  const projects = useSelector((state) => {
+    const projectList = Object.values(state.search);
+    return projectList?.map((project) => (
+      <ScrollTile
+        className="searchPage__project--scrollTile"
+        project={project}
+      />
+    ));
+  });
+
   return (
     <div>
       <SearchPageBar />
+      <div className="searchPage__projects--container">{projects}</div>
     </div>
   );
 }
