0d307b57e9535c10d4a4bd70ce6801250a3fd8fc
Chuks <chuksota@gmail.com>
2021-05-26 15:39:43 -0500

    added dates, and added new migration

--- app/models/project.py
+++ app/models/project.py
@@ -20,7 +20,8 @@ class Project(db.Model):
     city = db.Column(db.String(100), nullable=False)
     state = db.Column(db.String(50), nullable=False)
     zipcode = db.Column(db.Integer)
-
+    created_at = db.Column(db.DateTime())
+    expiration_date = db.Column(db.DateTime())
     donations = db.relationship("Donation", back_populates="project")
     user = db.relationship("User", back_populates="projects")
     state_relationship = db.relationship("State", back_populates="projects")
